dfebbe5085293f32d6a6777420bfd7e43d53d633
Austyn Krutsinger <akrutsinger@gmail.com>
2024-05-09 19:58:21 -1000

    Initial commit

---
+++ words.js
@@ -0,0 +1,10 @@
+export const NAMES = {
+	"duck": [
+		{"clue":"sticks anything together", "answer":"ducttape"},
+		{"clue":"helps you with your programming", "answer":"rubberduck"},
+	],
+	"cat": [
+		{"clue":"is a big, fancy boat", "answer":"catamaran"},
+		{"clue":"is a great and sudden suffering", "answer":"catastrophe"},
+	]
+}
